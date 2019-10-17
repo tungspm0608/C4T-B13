6742e91c5fb146530f899cd7e665c05113711980
tungspm0608 <tungspm0608@gmail.com>
2019-10-17 08:35:42 +0700

    nhap thang dua ra mua

---
+++ season 5/buoi5.py
@@ -0,0 +1,15 @@
+thang = int(input('thang may?'))
+if thang < 4:
+    print('muaxuan')
+elif thang < 7:
+    print('muahe')
+elif thang < 10:
+    print('muathu')
+elif thang < 13:
+    print ('muadong')      
+else:
+    print('khong la mua gi ca')    
+
+
+
+
