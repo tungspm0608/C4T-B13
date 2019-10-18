e8fe198c8004f356b67c2848690ddc5872d87db4
tungspm0608 <tungspm0608@gmail.com>
2019-10-18 21:44:09 +0700

    dua ngau nhien so in ra mua

---
+++ seasion   2/buoi5.py
@@ -0,0 +1,16 @@
+import random
+ngay = (random.randint(0, 100))
+print(ngay)
+if ngay <30:
+    print('rainny')
+elif 30 <= ngay <60:
+    print('cloudy')
+elif ngay >60:
+    print('sunny')
+            
+
+
+
+
+
+
