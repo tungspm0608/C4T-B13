5402a7e2c988d6b2dbbf9c5d41d2b4cd95bc19fb
tungspm0608 <tungspm0608@gmail.com>
2019-10-20 10:26:18 +0700

    mat khau2

--- seasion   2/buoi6.py
+++ seasion   2/buoi6.py
@@ -1,8 +1,12 @@
 while True:
     matkhau = input('what is your pass?')
     print(matkhau)
-    if matkhau.isdigit():
+    print(len(matkhau))
+    if len(matkhau) < 8:
+        print("ko du 8 ki tu loai")
+    elif matkhau.isdigit():
         print('dung roi')
+
         break   
     else:
         print("sai r nhap lai")
--- season 6/buoi6.py
+++ season 6/buoi6.py
@@ -1,8 +1,12 @@
 while True:
-    ten = input('what is your name?')
-    print(ten)
-    if ten.isalpha():
-        print('no la ten cua ban')
+    matkhau = input('what is your pass?')
+    print(matkhau)
+    print(len(matkhau))
+    if len(matkhau) < 8:
+        print("ko du 8 ki tu loai")
+    elif matkhau.isdigit():
+        print('dung roi')
+
         break   
     else:
         print("sai r nhap lai")
