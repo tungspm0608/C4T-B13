80dd957498b98b9c70414c0825a9cec5ad518563
tungspm0608 <tungspm0608@gmail.com>
2019-10-20 10:01:55 +0700

    nhap ten dung ay sai

---
+++ season 6/buoi6.py
@@ -0,0 +1,9 @@
+while True:
+    ten = input('what is your name?')
+    print(ten)
+    if ten.isalpha():
+        print('no la ten cua ban')
+        break   
+    else:
+        print("sai r nhap lai")
+
